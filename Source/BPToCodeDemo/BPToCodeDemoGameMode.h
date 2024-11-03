fceec1bb9d8a4029b19d57bfeeba2e1fba70aeac
gittheskit <ravimohan1991@gmail.com>
2024-11-03 16:06:03 +0530

    First project commit

---
+++ Source/BPToCodeDemo/BPToCodeDemoGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BPToCodeDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABPToCodeDemoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABPToCodeDemoGameMode();
+};
+
+
+
